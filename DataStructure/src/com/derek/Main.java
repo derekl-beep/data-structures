45d4a6b58c35a5a1d68d5067a8de79634f2dea50
Derek Lau <derek.lau.contact@gmail.com>
2020-07-08 22:07:55 +0800

    Update

--- DataStructure/src/com/derek/Main.java
+++ DataStructure/src/com/derek/Main.java
@@ -1,24 +1,10 @@
 package com.derek;
 
-import java.util.HashSet;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        PriorityQueue priorityQueue = new PriorityQueue();
-
-        priorityQueue.enqueue(1);
-        priorityQueue.enqueue(2);
-        priorityQueue.enqueue(4);
-        priorityQueue.enqueue(8);
-        priorityQueue.enqueue(16);
-        priorityQueue.enqueue(32);
-
-        priorityQueue.print();
-
-        System.out.println(priorityQueue.dequeue());
-        priorityQueue.print();
-
+        System.out.println(Thread.activeCount());
+        System.out.println(Runtime.getRuntime().availableProcessors());
     }
 }
